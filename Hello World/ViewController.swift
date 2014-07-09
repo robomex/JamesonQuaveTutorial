e861c9e05c265743f139c232ce6ef44b49a22a94
robomex <atmorales@gmail.com>
2014-07-09 01:31:42 -0500

    before hooking up table to requests

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableData: NSArray = []
+    @IBOutlet var appsTableView: UITableView
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("JQ Software")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10 // normally this - the number of rows in our section - would be the length of an array controller
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in println("Task completed")
+            if(error) {
+                // If there is an error in the web request, print it to the console
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            if (err?) {
+                // If there is an error parsing JSON, print it to the console 
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            var results: NSArray = jsonResult["results"] as NSArray
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableData = results
+                self.appsTableView.reloadData()
+                })
+        })
+        task.resume()
+    }
 
 }
 
